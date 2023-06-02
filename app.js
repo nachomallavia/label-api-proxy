ad397600fa2d3f719702e0f0e503f5e7110e71e4
nachomallavia <nachomallavia@hotmail.com>
2023-06-02 17:23:57 -0300

    meti el middleware para el CORS

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const https = require('https');
 
 const fetch = require('node-fetch');
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+
 async function handleLabelRequest(req,res){
     console.log('Trying to fetch label')
     try{let fullLabel = req.body;
