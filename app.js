1bd0849e9d5ba992557f3e7c11e6a7a062dc7707
nachomallavia <nachomallavia@hotmail.com>
2023-06-02 16:59:38 -0300

    primer intento de metodo post

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const port = 8080;
 const http =require('http');
 const https = require('https');
 
+const fetch = require('node-fetch');
+
+async function handleLabelRequest(req,res){
+    let fullLabel = req.body;
+    const response = await fetch('http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: fullLabel
+    });
+    res.send(response);
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World from RENDER!')
   })
 app.get('/check', (req,res)=>{
     res.send('Hola Hola')
 })
+app.post('/label',handleLabelRequest);
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
   })
